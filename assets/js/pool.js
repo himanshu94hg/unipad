7273662cd6b2b91cd31ce5293e30c26ea555d564
himanshu94hg <himanshugrover2710@gmail.com>
2021-09-25 22:07:52 +0530

    first commit

---
+++ assets/js/pool.js
@@ -0,0 +1,27 @@
+function myFunction1() {
+   document.getElementById("mylist1").classList.add("PoolNav_itemSelected__25MGl");
+   document.getElementById("mylist").classList.remove("PoolNav_itemSelected__25MGl");
+   document.getElementById("mylist2").classList.remove("PoolNav_itemSelected__25MGl");
+   
+   
+}
+
+function myFunction() {
+   document.getElementById("mylist").classList.add("PoolNav_itemSelected__25MGl");
+   document.getElementById("mylist1").classList.remove("PoolNav_itemSelected__25MGl");
+   document.getElementById("mylist2").classList.remove("PoolNav_itemSelected__25MGl");
+   
+   
+}
+
+
+function myFunction2() {
+   document.getElementById("mylist2").classList.add("PoolNav_itemSelected__25MGl");
+   document.getElementById("mylist").classList.remove("PoolNav_itemSelected__25MGl");
+   document.getElementById("mylist1").classList.remove("PoolNav_itemSelected__25MGl");
+   
+   
+}
+
+
+
